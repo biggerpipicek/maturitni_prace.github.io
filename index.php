2872134e732411b22f76f8fe82d3dfbbd41d0857
biggerpipicek <96359292+biggerpipicek@users.noreply.github.com>
2023-08-06 15:28:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>MATURITNÍ ZKOUŠKA</title>
+</head>
+<body>
+    <header>
+        <img src="logo_skoly.png" alt="Logo školy">
+    <p id="school_name"><b>Název školy:</b><br>Integrovaná střední škola Cheb</p>
+        <nav>
+            <a href="">O škole</a>
+            <a href="">Matematika</a>
+            <a href="">Čeština</a>
+            <a href="">Dějepis</a>
+            <a href="">Programování</a>
+            <a href="">Kontakty</a>
+            <a href="">APS</a>
+        </nav>
+    </header>
+    <div class="left">
+        <h1>MENU</h1>
+        <a href="php/scitani.php">- Sčítání</a><br><br><br><br>
+        <a href="php/nasobeni.php">- Násobení</a>
+    </div>
+
+    <div class="main">
+        <h1>Maturita 2015</h1>
+        <h1>Sčítání a násobení</h1>
+    </div>
+    <footer>
+        <p>© 2014, NÁZEV SPOLEČNOSTI - všechna práva vyhrazena<br> <a href="">Ochrana osobních údajů</a> | <a href="">Prohlášení o přístupnosti</a> | <a href="">Podmínky užití</a> | <a href="">Mapa stránek</a><br>Webové stránky vytvořil <a href="">XXXXX</a> | <a href="">XHTML 1.0</a> | <a href="">CSS 3</a></p>
+    </footer>
+</body>
+</html>
---
+++ php/scitani.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/table_form.css">
+    <title>Sčítání</title>
+</head>
+<body>
+    <div class="form">
+        <h1>SČÍTÁNÍ</h1>
+        <form action="" method="post">
+            <label for="input">Zadejte hodnotu X: <input type="number" name="input"></label>
+            <input type="submit" value="Odeslat!">
+        </form>
+    </div>
+
+    <div class="table">
+        <?php
+            if (!isset($_POST["input"]) || empty($_POST["input"])) {
+                echo "<p>Nezadal jste žádnou hodnotu! Zadejte prosím hodnotu od 1 do 9.</p>";
+            } elseif ($_POST["input"] > 0 && $_POST["input"] < 10) {
+                $x = $_POST['input'];
+                echo '<table>';
+                echo '<tr><th>č.p.</th><th>rovnice</th><th>výsledek</th></tr>';
+                for ($i = 1; $i <= 10; $i++) {
+                    $result = $x + $i;
+                    echo "<tr><th>$i.</th><td>$x + $i</td><td>$result</td></tr>";
+                }
+                echo '</table>';
+            } else {
+                echo "<p>Hodnota, kterou jste zadal, není platná. Zadejte prosím hodnotu od 1 do 9.</p>";
+            }
+        ?>
+    </div>
+</body>
+</html>
